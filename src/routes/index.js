e16ea5f517925cb839f4cbd1473e650ff10997f8
Jorge <jhessle04@gmail.com>
2020-02-13 16:59:05 -0300

    h

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,15 +2,79 @@ const router = require('express').Router();
 const nodemailer = require('nodemailer');
 
 router.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('indexesp');
 });
+
+router.get('/indexeng', (req, res, next) => {
+  res.render('indexeng');
+});
+
+
+
+router.get('/proyectouno', (req, res, next) => {
+  res.render('proyectos/proyectouno');
+});
+
+router.get('/proyectodos', (req, res, next) => {
+  res.render('proyectos/proyectodos');
+});
+
+router.get('/proyectotres', (req, res, next) => {
+  res.render('proyectos/proyectotres');
+});
+
+router.get('/proyectocuatro', (req, res, next) => {
+  res.render('proyectos/proyectocuatro');
+});
+
+router.get('/proyectocinco', (req, res, next) => {
+  res.render('proyectos/proyectocinco');
+});
+
+router.get('/proyectoseis', (req, res, next) => {
+  res.render('proyectos/proyectoseis');
+});
+
+
+
+
+
 
 
 router.get('/projectone', (req, res, next) => {
-  res.render('projectone');
+  res.render('projects/projectone');
+});
+
+router.get('/projecttwo', (req, res, next) => {
+  res.render('projects/projecttwo');
+});
+
+router.get('/projectthree', (req, res, next) => {
+  res.render('projects/projectthree');
+});
+
+router.get('/projectfour', (req, res, next) => {
+  res.render('projects/projectfour');
+});
+
+router.get('/projectfive', (req, res, next) => {
+  res.render('projects/projectfive');
+});
+
+router.get('/projectsix', (req, res, next) => {
+  res.render('projects/projectsix');
+});
+
+router.get('/projectseven', (req, res, next) => {
+  res.render('projects/projectseven');
 });
 
 
+
+
+router.get('/sobremi', (req, res, next) => {
+  res.render('sobremi');
+});
 
 router.get('/aboutme', (req, res, next) => {
   res.render('aboutme');
@@ -42,7 +106,7 @@ router.post('/email', async (req, res) => {
     service: 'gmail',
     auth: {
      user: 'lehj09@gmail.com',
-     pass: 'terremototo111'
+     pass: 'terremototo001'
      
    }
    });
@@ -50,8 +114,8 @@ router.post('/email', async (req, res) => {
    
    let mailOptions = {
     from: 'lehj09@gmail.com',
-    to: 'fhesslegrave@gmail.com',
-    subject: 'email node',
+    to: 'jhessle04@gmail.com',
+    subject: 'email website',
     html: contentHTML
    
    };
@@ -66,7 +130,7 @@ router.post('/email', async (req, res) => {
      console.log('Email sent: ' + info.response);
     }
    });
-   res.redirect('/contacto.hbs');
+   res.redirect('/contact');
 });
 
 
